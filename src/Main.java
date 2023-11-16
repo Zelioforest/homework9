d786936a09bbdf56aa45ac1235cc42c4d05c2d26
Zelioforest <veshky@mail.ru>
2023-11-17 00:19:52 +0300

    mistake

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,22 @@ public class Main {
     public static void main(String[] args) {
 
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        int[] arr = generateRandomArray();
+        int total = 0;
+        for (int i : arr) {
+            total += i;
+        }
+        System.out.println("Сумма трат за месяц составила " + total + " рублей");
+
+    }
+
 }
